17929390c3d0686e825fd98571f166e46eda4957
hcchoi <hcchoi@wbsoft.kr>
2020-10-15 10:15:22 +0900

    555221

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,10 @@
   devServer: { overlay: false },
   chainWebpack(config) {
     config.devtool('source-map')
-    config.output.filename = '[name].[hash].js'
-    config.output.chunkFilename = '[name].[hash].js'
   },
-
   css: {
     sourceMap : true,
-    extract: {
-    filename: '[name].css',
   },
-},
 
   publicPath: process.env.NODE_ENV === 'production'
       ? '/portfolio'
